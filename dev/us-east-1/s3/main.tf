dafae28c1a0824e43f6ddb3dd03499d2816c412f
agunta <abhilashgunta@gmail.com>
2021-07-27 15:03:51 -0700

    test2
    test2

--- dev/us-east-1/s3/main.tf
+++ dev/us-east-1/s3/main.tf
@@ -1,6 +1,11 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
 resource "aws_s3_bucket" "b" {
 bucket = "abilash-atlantistest-2"
-  acl    = "private"
+acl    = "private"
+region = "us-west-2"
 
   tags = {
     Name        = "abilash-atlantistest-2"
