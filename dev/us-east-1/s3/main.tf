236aa1f441e7aaec1b9f472505bf00b1ed67ee20
agunta <abhilashgunta@gmail.com>
2021-07-27 14:54:40 -0700

    test
    test

--- dev/us-east-1/s3/main.tf
+++ dev/us-east-1/s3/main.tf
@@ -1,2 +1,9 @@
-resource "null_resource" "b" {
+resource "aws_s3_bucket" "b" {
+bucket = "abilash-atlantistest-2"
+  acl    = "private"
+
+  tags = {
+    Name        = "abilash-atlantistest-2"
+    Environment = "Dev"
+  }
 }
